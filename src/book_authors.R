cf89fb04c69873ee5f47223222affab0b3a132cf
Timothy S Green <62939263+TSGreen@users.noreply.github.com>
2022-08-16 16:29:32 +0100

    refactor: Modularised code into funcs

--- src/book_authors.R
+++ src/book_authors.R
@@ -5,34 +5,61 @@ setwd("~/GitHub/books-of-the-world/src")
 library(tidyverse)
 library(janitor)
 
+get_read_books <- function(data, final_useful_column=19) {
+  #' Return dataframe of reading data with only read or currently reading
+  #' books selected and drop superfluous columns.
+  #' 
+  #' @param data The whole set of Goodreads user library data
+  #' @param final_useful_column The column number of the final useful column in data
+  #'
+  #' @return read_books Subet of books that have been read or are currently being read.
+
+  books_data <- data[1:final_useful_column]
+
+  # Select only read or currently reading books
+  read_books <- books_data %>% 
+    janitor :: clean_names() %>%
+    filter(
+      exclusive_shelf == "read" | 
+      exclusive_shelf == "currently-reading" 
+      )
+  return(read_books)
+}
+
+
+get_author_list <- function(data, literature_only=TRUE) {
+  #' Retrieve and save a list of read authors
+  #'
+  #' @param data Dataframe of read/currently reading books
+  #' @param literature_only If TRUE, selects subset of books that literature (rather
+  #' than non-fiction etc). 
+  #'
+  #' @return List of read authors and generates data file in data dir. 
+  
+  if (literature_only) { 
+    # Select only literature (rather than non-fiction etc).
+    # Relys on my shelving system of adding e.g. lit-south-asian or lit-european tags
+    data <- data %>%
+      filter(str_detect(bookshelves, "lit-" ))
+  }
+  
+  authors <- data$author %>% noquote() %>% unique()
+  
+  write.table(authors, 
+              file.path(data_path, "book_authors_list.csv"),
+              sep=",",
+              col.names=FALSE,
+              row.names=FALSE
+  )
+  
+  return(authors)
+}
+
 data_path = file.path("..", "data")
 
 books_data <- read_csv(file.path(data_path, "goodreads_library_export.csv"))
 
-# Strip out last several columns which are not needed
-final_useful_column <- 19
-books_data <- books_data[1:final_useful_column]
+read_books <- get_read_books(books_data)
 
-# Select only read or currently reading books
-read_books <- books_data %>% 
-  janitor :: clean_names() %>%
-  filter(
-    exclusive_shelf == "read" | 
-    exclusive_shelf == "currently-reading" 
-    )
-
-# Select only literature (rather than non-fiction etc).
-# Relys on my shelving system of adding e.g. lit-south-asian or lit-european tags
-read_lit <- read_books %>%
-  filter(str_detect(bookshelves, "lit-" ))
-
-authors <- read_lit$author %>% noquote() %>% unique()
-
-write.table(authors, 
-            file.path(data_path, "book_authors_list.csv"),
-            sep=",",
-            col.names=FALSE,
-            row.names=FALSE
-            )
-
+authors <- get_author_list(read_books)
 
